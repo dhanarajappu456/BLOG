a70d5bc477d21043b4126d909f46dc1ea3425da2
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-02-04 12:54:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -44,16 +44,37 @@ function generateToken(userName, userDoc, res) {
     }
   );
 }
-app.use(
-  cors({
-    credentials: true,
-    origin: "https://blog-front-roan.vercel.app",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "https://blog-front-roan.vercel.app",
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 
-  })
-);
+//   })
+// );
 
-// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://blog-front-roan.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
+
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
